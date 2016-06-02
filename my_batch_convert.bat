6febcd91433e9208ced69edb5f5446929b29e40f
MXI <KillyMXI@users.noreply.github.com>
2016-06-02 18:59:22 +0300

    Superellipse shapes added

--- my_batch_convert.bat
+++ my_batch_convert.bat
@@ -1,3 +1,4 @@
 @powershell.exe -NoProfile -ExecutionPolicy Bypass -file .\Convert-Shape.ps1 -in .\pages_photo -out .\mxi_shapes_photo -pretty
 @powershell.exe -NoProfile -ExecutionPolicy Bypass -file .\Convert-Shape.ps1 -in .\pages_rulers_and_measurements -out .\mxi_shapes_rulers_and_measurements -pretty
 @powershell.exe -NoProfile -ExecutionPolicy Bypass -file .\Convert-Shape.ps1 -in .\pages_misc_grids -out .\mxi_shapes_misc_grids -pretty
+@powershell.exe -NoProfile -ExecutionPolicy Bypass -file .\Convert-Shape.ps1 -in .\pages_superellipse -out .\mxi_shapes_superellipse -pretty
